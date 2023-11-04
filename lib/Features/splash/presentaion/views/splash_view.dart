7099a8a688b538b5e72466df3ef558bb2f2a9e9a
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-04 14:48:55 +0200

    Create splash body

--- lib/Features/splash/presentaion/views/splash_view.dart
+++ lib/Features/splash/presentaion/views/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/splash/presentaion/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -5,6 +6,8 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: SplashViewBody()
+    );
   }
 }
