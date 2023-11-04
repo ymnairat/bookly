003e217cef32434ee586d869dc55fbf520aecf51
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-04 20:26:13 +0200

    Navigate to home

--- lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/Features/home/presentaion/views/home.dart';
 import 'package:bookly/Features/splash/presentaion/views/widgets/sliding_text.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
@@ -17,6 +21,8 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   void initState() {
     super.initState();
     initSlidingAnimation();
+
+    navigateToHome();
   }
 
   @override
@@ -52,5 +58,19 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
             .animate(animationController);
             
     animationController.forward();
+  }  
+  
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(() => const HomeView(),
+            // calculations
+            transition: Transition.fade,
+            duration: kTranstionDuration);
+
+        //GoRouter.of(context).push(AppRouter.kHomeView);
+      },
+    );
   }
 }
