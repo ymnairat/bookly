bf745dabc86f20d3209b6c303a82a900cec3601c
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-05 15:52:39 +0200

    Add custom list view item

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/Features/home/presentaion/views/widgets/custom_app_bar.dart';
+import 'package:bookly/Features/home/presentaion/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
-
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -8,7 +8,9 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        CustomAppBar()
+        CustomAppBar(),
+        CustomListViewItem()
+
       ],
     );
   }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AssetsManager {
   static const logo = 'assets/images/Logo.png';
+  static const testImage = 'assets/images/test_image.png';
 }
