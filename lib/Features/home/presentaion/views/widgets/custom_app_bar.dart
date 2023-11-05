1bebdff2166934c59aa08fa4e7f3aa7fac249fa7
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-05 12:58:30 +0200

    Add font awesome & create custom app bar

---
+++ lib/Features/home/presentaion/views/widgets/custom_app_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsManager.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          IconButton(
+              onPressed: () {
+                // GoRouter.of(context).push(AppRouter.kSearchView);
+              },
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 22,
+              ))
+        ],
+      ),
+    );
+  }
+}
