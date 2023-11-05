5f697fb2dafbb620d4822ad18f14acc7890b9f54
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-05 23:29:44 +0200

    Best seller item p2

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:bookly/Features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentaion/views/widgets/featured_list_view.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -21,8 +23,58 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.textStyle18,
-            )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSellerViewItem()
         ],
+      ),
+    );
+  }
+}
+
+
+class BestSellerViewItem extends StatelessWidget {
+  const BestSellerViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsManager.testImage
+                    )
+                  )
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * .5,
+            child: const Column(
+              children: [
+                Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  )
+              ]
+            ),
+          )
+      ]
       ),
     );
   }
