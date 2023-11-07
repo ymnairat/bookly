eb5a00df162623c020b82f0af49434e182716b3a
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-07 10:13:33 +0200

    Create book details view

---
+++ lib/Features/home/presentaion/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Features/home/presentaion/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody()
+    );
+  }
+}
