2116a8f1b08dd7ff7c8a0c88504479cf935474bd
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-07 11:05:42 +0200

    Refactor nav

--- lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -1,10 +1,8 @@
-import 'package:bookly/Features/home/presentaion/views/home.dart';
 import 'package:bookly/Features/splash/presentaion/views/widgets/sliding_text.dart';
-import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:go_router/go_router.dart';
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
@@ -64,12 +62,12 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
     Future.delayed(
       const Duration(seconds: 2),
       () {
-        Get.to(() => const HomeView(),
-            // calculations
-            transition: Transition.fade,
-            duration: kTranstionDuration);
+        // Get.to(() => const HomeView(),
+        //     // calculations
+        //     transition: Transition.fade,
+        //     duration: kTranstionDuration);
 
-        //GoRouter.of(context).push(AppRouter.kHomeView);
+        GoRouter.of(context).push(AppRouter.kHomeView);
       },
     );
   }
---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/Features/home/presentaion/views/home.dart';
+import 'package:bookly/Features/splash/presentaion/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
+  static final router = GoRouter(
+      routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) => const SplashView(),
+          ),
+          GoRoute(
+            path: kHomeView,
+            builder: (context, state) => const HomeView(),
+          ),
+      ]
+    );
+}
