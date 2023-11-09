f173474eff583c57d003a631ac7cf1dad5ad1e39
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-09 10:39:10 +0200

    Create book entity & home repo

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity
+{
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity({this.authorName, this.image, this.price, this.rating, this.title})
+}
