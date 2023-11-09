68e413920422871176f47af783d6b7a86ecccac7
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-09 12:43:30 +0200

    fetch newest books use case

---
+++ lib/Features/home/domain/usecases/fetch_newest_books_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly/core/errors/Failure.dart';
+import 'package:bookly/core/use_case/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> // We can replace NoParam with void and remove (?)
+{
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase(this.homeRepo);
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async { // (?) check null safty because any object in dart is non nullable by default
+    return await homeRepo.fetchNewestBooks();
+  }
+}
+
+class NoParam {}
