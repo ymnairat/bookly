3662909c8396dec8f39c9bb00b715e3f2d82bb0a
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-12 22:39:16 +0200

    Create API service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiService
+{
+  final Dio _dio;
+  final String baseUrl = "https://www.googleapis.com/books/v1/";
+
+  ApiService(this._dio);
+
+  Future<Map<String, dynamic>> get({required Stream endpoint}) async {
+    var response = await _dio.get('$baseUrl$endpoint');
+    return response.data;
+  }
+}
